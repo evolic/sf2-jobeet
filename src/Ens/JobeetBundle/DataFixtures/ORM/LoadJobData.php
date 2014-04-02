a584dfe5378a7523e62ad8fcb456fae018945c3e
Tomasz Kuter <tkuter@grupainteger.pl>
2014-04-02 15:19:34 +0200

    - Added some expired job

--- src/Ens/JobeetBundle/DataFixtures/ORM/LoadJobData.php
+++ src/Ens/JobeetBundle/DataFixtures/ORM/LoadJobData.php
@@ -11,6 +11,22 @@ class LoadJobData extends AbstractFixture implements OrderedFixtureInterface
 {
   public function load(ObjectManager $em)
   {
+    $job_expired = new Job();
+    $job_expired->setCategory($em->merge($this->getReference('category-programming')));
+    $job_expired->setType('full-time');
+    $job_expired->setCompany('Sensio Labs');
+    $job_expired->setLogo('sensio-labs.gif');
+    $job_expired->setUrl('http://www.sensiolabs.com/');
+    $job_expired->setPosition('Web Developer Expired');
+    $job_expired->setLocation('Paris, France');
+    $job_expired->setDescription('Lorem ipsum dolor sit amet, consectetur adipisicing elit.');
+    $job_expired->setHowToApply('Send your resume to lorem.ipsum [at] dolor.sit');
+    $job_expired->setIsPublic(true);
+    $job_expired->setIsActivated(true);
+    $job_expired->setToken('job_expired');
+    $job_expired->setEmail('job@example.com');
+    $job_expired->setCreatedAt(new \DateTime('2005-12-01'));
+
     $job_sensio_labs = new Job();
     $job_sensio_labs->setCategory($em->merge($this->getReference('category-programming')));
     $job_sensio_labs->setType('full-time');
@@ -25,7 +41,7 @@ class LoadJobData extends AbstractFixture implements OrderedFixtureInterface
     $job_sensio_labs->setIsActivated(true);
     $job_sensio_labs->setToken('job_sensio_labs');
     $job_sensio_labs->setEmail('job@example.com');
-    $job_sensio_labs->setExpiresAt(new \DateTime('2012-10-10'));
+    $job_sensio_labs->setExpiresAt(new \DateTime('2014-04-10'));
 
     $job_extreme_sensio = new Job();
     $job_extreme_sensio->setCategory($em->merge($this->getReference('category-design')));
@@ -41,8 +57,9 @@ class LoadJobData extends AbstractFixture implements OrderedFixtureInterface
     $job_extreme_sensio->setIsActivated(true);
     $job_extreme_sensio->setToken('job_extreme_sensio');
     $job_extreme_sensio->setEmail('job@example.com');
-    $job_extreme_sensio->setExpiresAt(new \DateTime('2012-10-10'));
+    $job_extreme_sensio->setExpiresAt(new \DateTime('2014-04-10'));
 
+    $em->persist($job_expired);
     $em->persist($job_sensio_labs);
     $em->persist($job_extreme_sensio);
 
