243ee31d1a9d9e8c9804682f6601cacc44b92f74
Tomasz Kuter <tkuter@grupainteger.pl>
2014-04-02 12:29:14 +0200

    - Added basic CRUD support for Job entity by using following command:   `php app/console doctrine:generate:crud --entity=EnsJobeetBundle:Job --route-prefix=ens_job --with-write --format=yml`

--- src/Ens/JobeetBundle/Controller/DefaultController.php
+++ src/Ens/JobeetBundle/Controller/DefaultController.php
@@ -8,6 +8,12 @@ class DefaultController extends Controller
 {
     public function indexAction($name)
     {
+       $logger = $this->get('logger');
+       $logger->info(__METHOD__);
+       $logger->notice('sample notice', $_SERVER);
+       $logger->warn('sample warning', $_COOKIE);
+       $logger->error('sample error', array(1, 2, 3));
+
         return $this->render('EnsJobeetBundle:Default:index.html.twig', array('name' => $name));
     }
 }
